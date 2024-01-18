61cb8e5d598e887e01ea7480b5497fbc9e8da381
Cauê <cauusaantos@gmail.com>
2024-01-17 21:24:47 -0300

    Arquivos

---
+++ Codes/Aimbot_tst/aimbot.py
@@ -0,0 +1,24 @@
+import pyautogui
+pyautogui.sleep(2)
+
+cor = (251, 0, 7)
+regiao = (735, 386, 1168, 822)
+
+for i in range(100):
+    pyautogui.sleep(0.0001)
+    pyautogui.moveTo(937, 245)
+    px_color = pyautogui.pixel(800, 500)
+    cord = pyautogui.locateOnScreen(r'C:\Users\CAUÊ\Documents\PyAutoGui\Testes\Aimbot\alvo.png', region=(735, 386, 1168, 822))
+
+    if cord:
+        pyautogui.click(cord, button='left')
+    
+    elif px_color == cor:
+        pyautogui.click(cor, button='left')
+    
+    pyautogui.sleep(0.00001)
+
+# site : https://www.arealme.com/aim-test/pt/#google_vignette
+    
+    ''' Script Criado com o intuito de simular um aimbot 
+        em um game de reflexo'''
--- Testes/Aimbot/aimbot_aimbooster.py
+++ Testes/Aimbot/aimbot_aimbooster.py
@@ -1,25 +1,28 @@
 import pyautogui
 import keyboard
-import time
+import win32api
+import win32con
 
-def click(x, y):
-    pyautogui.click(x, y)
+def click2(x, y):
+    win32api.SetCursorPos((x, y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
 
 while not keyboard.is_pressed('c'):
     print('Começou a percorrer o SC')
     sc = pyautogui.screenshot(region=(0, 0, 1920, 1080))
     width, height = sc.size
 
-    for x in range(0, width, 7):
+    for x in range(0, width, 17):
         achou = 0
 
-        for y in range(0, height, 7):
+        for y in range(0, height, 17):
             r, g, b = sc.getpixel((x, y))
             print(r, g, b)
 
             if r == 255 and g == 219 and b == 195:
                 print('ok')
-                click(x, y)
+                click2(0 + x, 0 + y)
                 achou = 1
                 break
 

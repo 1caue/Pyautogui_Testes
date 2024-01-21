f91e55980d7a85abe51209cffcc207ca01afbe08
Cauê <cauusaantos@gmail.com>
2024-01-20 21:07:39 -0300

    Arquivos

--- Projeto/cap.py
+++ Projeto/cap.py
@@ -1,20 +1,21 @@
 from pyautogui import *
 
-moveTo(966, 1076)
 hotkey('win', 'd')
 sleep(2)
-
-img = locateOnScreen(r'C:\Users\CAUÊ\Documents\PyAutoGui\Projeto\img.png')
+click(966, 1076)
 
 try:
+    img = locateOnScreen(r'C:\Users\CAUÊ\Documents\PyAutoGui\Projeto\att.png')
     
-    if img:
+    if img is not None:
         print('tudo ok')
-        prin = screenshot(region=(0, 0, 344, 1026))
-        prin.show()
-    else:
-        print('imagem não encontrada')
 
-except ImageNotFoundException:
-    print('Erro')
-        
+except Exception as e:
+   print('Mudanças foram feitas!')
+   prin = screenshot(region=(0, 0, 1914, 1034))
+   prin.show()
+
+finally:
+    sleep(5)
+    hotkey('win', 'd')
+    

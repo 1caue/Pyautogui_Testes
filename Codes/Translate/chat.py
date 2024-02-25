a82c0702ad7052d2442e11dbd12f519fa76a0ba8
Cauê <cauusaantos@gmail.com>
2024-02-24 21:19:09 -0300

    refactor

--- Codes/Translate/chat.py
+++ Codes/Translate/chat.py
@@ -4,33 +4,43 @@ from selenium.webdriver.common.by import By
 from pyautogui import *
 
 user = input('O que deseja traduzir?: ')
+save = input('Deseja Salvar o Arquivo .txt Como?: ')
 
-pacote = r'C:\Users\CAUÊ\Documents\PyAutoGui\Codes\chromedriver.exe'
-chrome_service = Service(pacote)
-chrome_options = webdriver.ChromeOptions()
+try:
+    pacote = 'chromedriver.exe'
+    chrome_service = Service(pacote)
+    chrome_options = webdriver.ChromeOptions()
 
-driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+    driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
-driver.get('https://translate.google.com.br/?hl=pt-BR')
-hotkey('win', 'up')
-sleep(1)
+    driver.get('https://translate.google.com.br/?hl=pt-BR')
+    hotkey('win', 'up')
+    sleep(1)
 
-write(user)
+    write(user)
 
-sleep(4)
-copy = driver.find_element(By.XPATH, '/html/body/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[2]/div/div[6]/div/div[6]/div[2]/span[2]/button/div[3]')
-copy.click()
+    sleep(4)
+    copy = driver.find_element(By.XPATH, '/html/body/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[2]/div/div[6]/div/div[6]/div[2]/span[2]/button/div[3]')
+    copy.click()
 
-sleep(1)
-click(503, 1056)
-hotkey('win', 'up')
+    sleep(1)
+    doubleClick(504, 1052)
+    sleep(0.5)
+    hotkey('win', 'up')
 
-sleep(1)
-hotkey('ctrl', 'v')
+    sleep(1)
+    hotkey('ctrl', 'v')
 
-sleep(1)
-hotkey('ctrl', 's')
+    sleep(1)
+    hotkey('ctrl', 's')
 
-sleep(60)
+    sleep(1)
+    write(save)
+    press('enter')
+
+    sleep(60)
+
+except Exception as e:
+    print(f'Ocorreu um erro de {e}')
 
 'Incompleto'

4fb66bed53dbb0a8ae2dd3fec09d87891b836c23
Cauê <cauusaantos@gmail.com>
2024-02-28 22:12:45 -0300

    feat

--- Codes/interface/interface.py
+++ Codes/interface/interface.py
@@ -1,26 +1,28 @@
 from tkinter import *
-from functools import partial 
-from pyautogui import *
+from defs import *
 
 interface = Tk()
-interface.title('Auto-Tasks')
+interface.title('windows 10 pro')
 
-def command_click(x, y):
-    click(x, y)
+btn_click(interface, "Minimizar Tela", 20, 20, 65, command_click, 1799, 11)
 
-def command_write(word):
-    write('ola mundo')
+btn_click(interface, "Entrar nos aplicativos", 20, 20, 95, command_click, 24, 1054)
 
-def btn(a, b, w, x, y, cmd):
-    bt = Button(a, width=w, text=b, command=partial(cmd, 1799, 11))
-    bt.place(x=x, y=y)
+btn_click(interface, "Barra de pesquisa", 20, 20, 125, command_click, 114, 1057)
 
-def frase(a, b, x, y):
-    lb = Label(a, text=b)
-    lb.place(x=x, y=y)
+escrever(interface, "Escrever", 20, 20, 155, write, 'ola mundo')
 
-btn(interface, "Minimizar Tela", 20, 20, 65, command_click)
-btn(interface, "Minimizar Tela", 20, 20, 65, command_write)
+pressionar(interface, "Comando", 20, 20, 185, 'f10')
+
+pressionar2(interface, "Abrir Executador", 20, 20, 215, 'win', 'r')
+
+pressionar2(interface, "Ver Janelas Abertas", 22, 175, 155, 'win', 'tab')
+
+pressionar2(interface, "Abrir Explorador de arquivos", 22, 175, 185, 'win', 'e')
+
+pressionar2(interface, "Abrir Configurações", 20, 175, 215, 'win', 'i')
+
+pressionar2(interface, "Fechar", 20, 1, 290, 'alt', 'f4')
 
 interface.geometry("370x320+200+500")
 interface.mainloop()
